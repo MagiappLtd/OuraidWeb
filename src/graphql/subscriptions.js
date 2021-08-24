61a6ebe821490a20d2b779819b898c6842819ce1
MagiappLtd <magic.app.Ink@gmail.com>
2021-08-24 18:36:37 +0900

    test

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo($usermname: String) {
+    onCreateTodo(usermname: $usermname) {
+      id
+      name
+      email
+      description
+      createdAt
+      updatedAt
+      usermname
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo($usermname: String) {
+    onUpdateTodo(usermname: $usermname) {
+      id
+      name
+      email
+      description
+      createdAt
+      updatedAt
+      usermname
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo($usermname: String) {
+    onDeleteTodo(usermname: $usermname) {
+      id
+      name
+      email
+      description
+      createdAt
+      updatedAt
+      usermname
+    }
+  }
+`;
