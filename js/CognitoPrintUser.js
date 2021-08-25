93b5842d1be689ec494841e72217106e2f5fecaa
MagiappLtd <magic.app.Ink@gmail.com>
2021-08-26 05:32:54 +0900

    d

--- js/CognitoPrintUser.js
+++ js/CognitoPrintUser.js
@@ -1,17 +1,13 @@
-// ユーザープールの設定
-const poolData = {	
-       UserPoolId: 'ap-northeast-1_WwnLoFfMf', // Your user pool id here
+(() => {
+  // ユーザープールの設定
+  const poolData = {
+        UserPoolId: 'ap-northeast-1_WwnLoFfMf', // Your user pool id here
         ClientId: '19jusin3gl62f049g3iukarni5' // Your client id here
-};
-const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-const cognitoUser = userPool.getCurrentUser();  // 現在のユーザー
+  };
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  const cognitoUser = userPool.getCurrentUser(); // 現在のユーザー
 
-var currentUserData = {};  // ユーザーの属性情報
-
-/**
- * 画面読み込み時の処理
- */
-$(document).ready(function() {
+  const currentUserData = {}; // ユーザーの属性情報
 
  // Amazon Cognito 認証情報プロバイダーを初期化します
 AWS.config.region = 'ap-northeast-1'; // リージョン
@@ -19,38 +15,44 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'ap-northeast-1:25aed12b-aa11-4c53-81c4-97c0b21d7daa',
 });
 
-		    
-    // 現在のユーザーの属性情報を取得・表示
-    getUserAttribute();
-});
 
-/**
- * 現在のユーザーの属性情報を取得・表示する
- */
-var getUserAttribute = function(){
-	
-    // 現在のユーザー情報が取得できているか？
-    if (cognitoUser != null) {
-        cognitoUser.getSession(function(err, session) {
-            if (err) {
-                console.log(err);
-                $(location).attr("href", "index.html");
-            } else {
-                // ユーザの属性を取得
-                cognitoUser.getUserAttributes(function(err, result) {
-                    if (err) {
-                        $(location).attr("href", "index.html");
-                    }
-                    
-                    // 取得した属性情報を連想配列に格納
-                    for (i = 0; i < result.length; i++) {
-                        currentUserData[result[i].getName()] = result[i].getValue();
-                    }
-                    $("div#menu h1").text("ようこそ！" + currentUserData["name"] + "さん");
-                });
-            }
+  // 現在のユーザーの属性情報を取得・表示する
+  // 現在のユーザー情報が取得できているか？
+  if (cognitoUser != null) {
+    cognitoUser.getSession((err, session) => {
+      if (err) {
+        console.log(err);
+      //  location.href = "signin.html";
+      } else {
+        // ユーザの属性を取得
+        cognitoUser.getUserAttributes((err, result) => {
+          if (err) {
+      //      location.href = "signin.html";
+          }
+
+          // 取得した属性情報を連想配列に格納
+          for (i = 0; i < result.length; i++) {
+            currentUserData[result[i].getName()] = result[i].getValue();
+          }
+          document.getElementById("name").innerHTML =
+            "ようこそ！" + currentUserData["name"] + "さん";
+          document.getElementById("role").innerHTML =
+            "Your Role is " + currentUserData["custom:role"];
+          document.getElementById("email").innerHTML =
+            "Your E-Mail is " + currentUserData["email"];
+
+          // サインアウト処理
+          const signoutButton = document.getElementById("signout");
+          signoutButton.addEventListener("click", event => {
+            cognitoUser.signOut();
+            location.reload();
+          });
+          signoutButton.hidden = false;
+          console.log(currentUserData);
         });
-    } else {
-        $(location).attr("href", "index.html");
-    }
-};
+      }
+    });
+  } else {
+ //   location.href = "signin.html";
+  }
+})();
