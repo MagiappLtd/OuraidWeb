463dfdfdf2889ed9c4a105fdf7efc9cb8c878762
MagiappLtd <magic.app.Ink@gmail.com>
2021-08-25 23:13:21 +0900

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,24 +1,55 @@
-function OnCognitoSignUp() {
-
-    var poolData = {
+// ユーザープールの設定
+const poolData = {
         UserPoolId: 'ap-northeast-1_5tA85AbZd', // Your user pool id here
         ClientId: '6d47bd9cp0d7tj03gvonvt0d7', // Your client id here
-    };
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-    var nickname = document.getElementById("nickname").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    userPool.signUp(nickname,email, password, null, null, function(
-        err,
-        result
-    ) {
-        if (err) {
-            alert(err.message || JSON.stringify(err));
-            return;
-        }
-        var cognitoUser = result.user;
-        console.log('user name is ' + cognitoUser.getUsername());
+};
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+ 
+var attributeList = [];
+ 
+/**
+ * 画面読み込み時の処理
+ */
+$(document).ready(function() {
+		
+	// Amazon Cognito 認証情報プロバイダーの初期化
+	AWSCognito.config.region = 'ap-northeast-1'; // リージョン
+	AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
+	    IdentityPoolId:'ap-northeast-1:ef9895ab-2704-4c5e-aac7-868f821f54fc';
+	});
+		    
+	// 「Create Account」ボタン押下時
+	$("#createAccount").click(function(event) {
+	    signUp();
+	});
+});
+ 
+/**
+ * サインアップ処理。
+ */
+var signUp = function() {
+			
+	var username = $("#email").val();
+	var nickname = $("#nickname").val();
+	var password = $("#password").val();
+			
+	// 何か1つでも未入力の項目がある場合、処理終了
+    if (!username | !nickname | !password) { 
+    	return false; 
+    }
+		    
+ 
+	var attributeFamilyName = new AmazonCognitoIdentity.CognitoUserAttribute(dataFamilyName);
+	var attributeGivenName = new AmazonCognitoIdentity.CognitoUserAttribute(dataGivenName);
+			
+			
+    // サインアップ処理
+    userPool.signUp(username, password, nickname, null, function(err, result){
+	    if (err) {
+	    	alert(err);
+			return;
+	    } else {
+	      	// サインアップ成功の場合、アクティベーション画面に遷移する
+	    }
     });
 }
