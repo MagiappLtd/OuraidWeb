2a5532a6cfc2608d5100e892e573234d4eb472b6
MagiappLtd <magic.app.Ink@gmail.com>
2021-08-25 23:28:50 +0900

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,12 +1,12 @@
 // ユーザープールの設定
 const poolData = {
-        UserPoolId: 'ap-northeast-1_5tA85AbZd', // Your user pool id here
-        ClientId: '6d47bd9cp0d7tj03gvonvt0d7', // Your client id here
+    UserPoolId :'ap-northeast-1_5tA85AbZd',
+    ClientId : '6d47bd9cp0d7tj03gvonvt0d7'
 };
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
- 
+
 var attributeList = [];
- 
+
 /**
  * 画面読み込み時の処理
  */
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	// Amazon Cognito 認証情報プロバイダーの初期化
 	AWSCognito.config.region = 'ap-northeast-1'; // リージョン
 	AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
-	    IdentityPoolId:'ap-northeast-1:ef9895ab-2704-4c5e-aac7-868f821f54fc'
+	    IdentityPoolId: [IDプールのID]
 	});
 		    
 	// 「Create Account」ボタン押下時
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	    signUp();
 	});
 });
- 
+
 /**
  * サインアップ処理。
  */
@@ -34,17 +34,21 @@ var signUp = function() {
 	var password = $("#password").val();
 			
 	// 何か1つでも未入力の項目がある場合、処理終了
-    if (!username | !nickname | !password) { 
+    if (!username | !lastName | !firstName | !password) { 
     	return false; 
     }
 		    
- 
+    // ユーザ属性リストの生成
+	var dataFamilyName = {
+		Name : "nickname",
+		Value : nickname
+	}
 	var attributeFamilyName = new AmazonCognitoIdentity.CognitoUserAttribute(dataFamilyName);
-	var attributeGivenName = new AmazonCognitoIdentity.CognitoUserAttribute(dataGivenName);
 			
+    attributeList.push(attributeFamilyName);
 			
     // サインアップ処理
-    userPool.signUp(username, password, nickname, null, function(err, result){
+    userPool.signUp(username, password, attributeList, null, function(err, result){
 	    if (err) {
 	    	alert(err);
 			return;
