ed159dbccdff1d86955eb1af75ff5c4fa3bf093e
MagiappLtd <magic.app.Ink@gmail.com>
2021-08-27 01:27:55 +0900

    test

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "ouraid@magiappltd.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
