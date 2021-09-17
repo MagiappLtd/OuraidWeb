c5f4930140b6a3f0fef7be11ced2027c6c64f358
MagiappLtd <magic.app.Ink@gmail.com>
2021-09-18 05:22:17 +0900

    f

---
+++ MyPage/examples/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import TextEditor from "./TextEditor";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TextEditor />
+    </div>
+  );
+}
